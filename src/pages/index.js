db6eb36158a639687daf15e0e6ab327221b11fdb
mativiscusso <mativiscusso@gmail.com>
2020-10-12 09:47:20 -0300

    agregando componentes estructurales sin contenido

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/layout/layout.js"
 import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/Seo"
 
 const IndexPage = () => (
   <Layout>
